73d4b7d2adc8d3fdabdf6f30307a5067b4d3a928
Sankalp Vats <sankalp.vats.che24@itbhu.ac.in>
2026-07-01 01:54:17 +0530

    Use Firebase Admin environment variables

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,20 +1,11 @@
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-} from "firebase/auth";
+const admin = require("firebase-admin");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDDC_5EmO8RK4W-EpleM_NJkjqC984284o",
-  authDomain: "codesync-773a3.firebaseapp.com",
-  projectId: "codesync-773a3",
-  storageBucket: "codesync-773a3.firebasestorage.app",
-  messagingSenderId: "479788547903",
-  appId: "1:479788547903:web:938970905ce4670af01d8a",
-};
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-
-export const provider = new GoogleAuthProvider();
+module.exports = admin;
